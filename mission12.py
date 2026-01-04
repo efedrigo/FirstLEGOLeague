385659cebf1b104c1aa7f5650ac255c9e249e9d8
Enrico Fedrigo <enrico-git@fedrigo-costa.eu>
2026-01-04 13:14:14 +0100

    mission12 completa

--- mission12.py
+++ mission12.py
@@ -6,7 +6,7 @@ import umath as umath
 # 200 no, 250 no, 300 no, 500 no, 450 no, 400?
 # Speed levels (mm/s)
 FAST_SPEED = 500
-MEDIUM_FAST_SPEED=200
+MEDIUM_FAST_SPEED=320
 MEDIUM_SPEED = 300
 SLOW_SPEED = 100
 DISTANCE1 = 450
@@ -16,13 +16,13 @@ def mission12(myRobot):
     # accessory direction signs (start with negative as requested)
     accessory_left_sign = -1
     accessory_right_sign = 1
-
+ 
     # Initialize DriveBase (pass gyro if available)
     drive = myRobot.driveBase
 
     drive.reset()  # zero distance measurement
     drive.use_gyro(True)
-    drive.settings(straight_speed=MEDIUM_FAST_SPEED, straight_acceleration=2 * MEDIUM_SPEED)
+    drive.settings(straight_speed=MEDIUM_SPEED, straight_acceleration=2 * MEDIUM_SPEED)
 
     drive.straight(DISTANCE1, then=Stop.BRAKE, wait=True)
     wait(0)
@@ -32,14 +32,17 @@ def mission12(myRobot):
     
     drive.straight(10, then=Stop.BRAKE, wait=True)
     wait(10)
-    drive.turn(5, then=Stop.BRAKE, wait=True) 
-    drive.straight(20, then=Stop.BRAKE, wait=True)
-    drive.turn(3, then=Stop.BRAKE, wait=True)
-    drive.straight(17, then=Stop.BRAKE, wait=True)
-    drive.turn(3, then=Stop.BRAKE, wait=True)
-    drive.straight(50, then=Stop.BRAKE, wait=True)
+    drive.turn(3) 
+    drive.straight(20)
+    drive.turn(3)
+    drive.straight(3)
+    drive.turn(3)
+    drive.straight(70)
+    drive.turn(20, then=Stop.BRAKE, wait=True)
     drive.settings(straight_speed=FAST_SPEED, straight_acceleration=2 * FAST_SPEED)
-    #drive.straight(-DISTANCE2)
+    
+    wait(10)
+    drive.straight(-DISTANCE2)
 
     #color sensor = very big problem
     return
