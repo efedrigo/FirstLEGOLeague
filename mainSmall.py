2d5bb6815d109028aa3af32e4364796a979178a9
Lorenzo Tonet <lotonet@posteo.net>
2026-01-12 16:42:46 +0100

    mainsmall

--- mainSmall.py
+++ mainSmall.py
@@ -6,13 +6,13 @@ from pybricks.tools import wait, StopWatch, multitask, run_task
 from pybricks.parameters import Axis
 import umath
 
-
 from robotcompact import robotCompetitionCompact
 #from odometer import odometer
 
 from testConfig import program1, program0
 from testOdometer import program2
 from testPursuit import testPursuit
+
 from mission1 import mission1
 from mission3_4 import mission3_4
 from mission9 import mission9
@@ -21,6 +21,42 @@ from mission8 import mission8
 from mission10 import mission10
 from mission12 import mission12
 from mission956 import mission956
+
+### START REPEATED BLOCK
+### THIS IS REPEATED HERE BECAUSE IT WAS OVERWRITTEN BY IMPORTING FROM testConfig.py 
+Color.LIGHTGREEN = Color(h=72, s=46, v=13)
+Color.DARKGREEN = Color(h=135, s=51, v=9)
+Color.MAGENTA = Color(h=260, s=51, v=5)
+Color.FUCSIA = Color(h=338, s=81, v=9)
+Color.ROSE = Color(h=324, s=46, v=32) # redefined
+Color.RED = Color(h=348, s=88, v=15)
+Color.DARKBLUE = Color(h=213, s=87, v=13) # redefined
+Color.LIGHTBLUE = Color(h=208, s=71, v=17)
+Color.YELLOW = Color(h=49, s=65, v=29) # redefined
+Color.LIGHTBROWN = Color(h=0, s=65, v=11)
+Color.DARKBROWN = Color(h=345, s=64, v=5)
+Color.BEIGE = Color(h=21, s=49, v=19)
+Color.BLACK = Color(h=240, s=22, v=5) # redefined
+Color.WHITE = Color(h=180, s=3, v=32) # redefined
+Color.LIGHTGRAY = Color(h=270, s=20, v=13)
+Color.NONE = Color(h=330, s=88, v=1)
+my_colors = (Color.LIGHTGREEN, 
+             Color.DARKGREEN, 
+             Color.MAGENTA, 
+             Color.FUCSIA, 
+             Color.ROSE, 
+             Color.RED, 
+             Color.DARKBLUE,
+             Color.LIGHTBLUE,
+             Color.YELLOW,
+             Color.LIGHTBROWN,
+             Color.DARKBROWN,
+             Color.BEIGE,
+             Color.BLACK,
+             Color.WHITE,
+             Color.LIGHTGRAY,
+             Color.NONE)
+### END REPEATED BLOCK
 
 watch = StopWatch()
 myRobot = robotCompetitionCompact()
