a57edaf1cca52206d84255c1fc3c196918b775d9
Enrico Fedrigo <enrico-git@fedrigo-costa.eu>
2026-01-13 18:30:17 +0100

    mission12

--- mission12.py
+++ mission12.py
@@ -10,7 +10,7 @@ FAST_SPEED = 500
 MEDIUM_FAST_SPEED=320
 MEDIUM_SPEED = 300
 SLOW_SPEED = 100
-DISTANCE1 = 420
+DISTANCE1 = 475
 DISTANCE2 = 100
 
 def mission12(myRobot):
@@ -24,35 +24,25 @@ def mission12(myRobot):
 
     drive.reset()  # zero distance measurement
     drive.use_gyro(True)
-    drive.settings(straight_speed=FAST_SPEED, straight_acceleration=2 * FAST_SPEED)
-    drive.settings(turn_rate=150, turn_acceleration=200)
+    drive.settings(straight_speed=MEDIUM_FAST_SPEED, straight_acceleration=2 * MEDIUM_FAST_SPEED,turn_rate=150, turn_acceleration=200)
     drive.straight(DISTANCE1, then=Stop.BRAKE, wait=True)
- #   wait(0)
-    drive.turn(5, then=Stop.BRAKE, wait=True) 
+    drive.straight(-10, then=Stop.BRAKE, wait=True)
+    myRobot.rotateAbs(0)
   
  #   wait(0.000000001)
     
     drive.settings(straight_speed=SLOW_SPEED, straight_acceleration=2 * SLOW_SPEED)
     i=0
-    while i<4:
+    while i<3:
         print("i:", i)
-        drive.straight(40, then=Stop.BRAKE, wait=True)
-        drive.turn(5)
+        drive.drive(MEDIUM_SPEED,0) # speed,turn rate
+        wait(300)
+#        drive.straight(40, then=Stop.BRAKE, wait=True)
+#        drive.turn(5, then=Stop.BRAKE, wait=True) 
         i+=1 
-
     drive.straight(-DISTANCE2)
+    myRobot.rotateAbs(0)
     drive.settings(straight_speed=FAST_SPEED, straight_acceleration=2 * FAST_SPEED)
-    drive.straight(-DISTANCE1)
+    drive.straight(-DISTANCE1+DISTANCE2)
     return
                     
-    drive.straight(3)
-    drive.turn(3)
-    drive.straight(70)
-    drive.turn(20, then=Stop.BRAKE, wait=True)
-    drive.settings(straight_speed=FAST_SPEED, straight_acceleration=2 * FAST_SPEED)
-    
-    wait(10)
-    drive.straight(-DISTANCE2)
-
-    #color sensor = very big problem
-    return
